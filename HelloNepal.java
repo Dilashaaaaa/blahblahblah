d69ff9edcd10b2d20c65c192f0d8e5ddc16e5201
Blahhh <np01cp4a250009@islingtoncollege.edu.np>
2025-11-20 10:28:42 +0545

    hii

--- HelloNepal.java
+++ HelloNepal.java
@@ -10,8 +10,8 @@ public class HelloNepal
     public static void main(String[] args){
         
   int a=5;
-  int b=10;
-  int c=5+10;
+  int b=15;
+  int c=5+15;
 System.out.println(c);
 System.out.println(a<b);
 
